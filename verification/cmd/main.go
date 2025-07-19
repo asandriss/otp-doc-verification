5a28a01319d531c17b194822664c640ea2746cce
Fedor Hajdu <fedorhajdu@gmail.com>
2025-07-19 17:00:49 +0100

    dockerize the services

---
+++ verification/cmd/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"net"
+	"net/http"
+
+	"github.com/asandriss/otp-doc-verification/shared/config"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	cfg := config.Config{}
+
+	if err := config.LoadConfig(&cfg); err != nil {
+		log.Fatalf("failed to load config: %v", err)
+	}
+
+	router := gin.Default()
+	router.GET("/health", getHealth)
+
+	ip := "0.0.0.0"
+	addr := net.JoinHostPort(ip, cfg.Port)
+	router.Run(addr)
+}
+
+func getHealth(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"status":  "ok",
+		"service": "verification",
+	})
+}
